27185024a6dd9602ea89827ff4e97ee64b339eeb
Pipat Posawad <pipat@mycostech.com>
2016-09-14 16:47:32 +0700

    fix RdnRmnOd Handler

--- src/Chronic/Handlers/RdnRmnOdHandler.cs
+++ src/Chronic/Handlers/RdnRmnOdHandler.cs
@@ -1,5 +1,6 @@
 using System;
 using Chronic.Tags.Repeaters;
+using System.Linq;
 
 namespace Chronic.Handlers
 {
@@ -10,6 +11,7 @@ namespace Chronic.Handlers
         {
             var month = tokens[1].GetTag<RepeaterMonthName>();
             var day = tokens[2].GetTag<OrdinalDay>().Value;
+            var time_tokens = tokens.Skip(3).ToList();
             var year = options.Clock().Year;
 
             if (Time.IsMonthOverflow(year, (int) month.Value, day))
@@ -18,9 +20,17 @@ namespace Chronic.Handlers
             }
             try
             {
-                var start = Time.New(year, (int) month.Value, day);
-                var end = start.AddDays(1);
-                return new Span(start, end);
+                if(time_tokens == null || time_tokens.Count == 0)
+                {
+                    var start = Time.New(year, (int)month.Value, day);
+                    var end = start.AddDays(1);
+                    return new Span(start, end);
+                }
+                else
+                {
+                    var dayStart = Time.New(year, (int)month.Value, day);
+                    return Utils.DayOrTime(dayStart, time_tokens, options);
+                }
             }
             catch (ArgumentException)
             {
